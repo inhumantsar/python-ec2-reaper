c91094f13c2ad185fd792996fa701ac8bf7957b6
Shaun Martin <shaun@samsite.ca>
2017-12-13 16:14:32 -0500

    fix for python 3.x

--- ec2_reaper/__init__.py
+++ ec2_reaper/__init__.py
@@ -2,8 +2,13 @@
 
 """Top-level package for EC2 Reaper."""
 
+import sys
+
 __author__ = """Shaun Martin"""
 __email__ = 'shaun@samsite.ca'
 __version__ = '0.1.3'
 
-from ec2_reaper import reap
+if sys.version_info >= (3, 0):
+    import ec2_reaper
+else:
+    from ec2_reaper import reap
